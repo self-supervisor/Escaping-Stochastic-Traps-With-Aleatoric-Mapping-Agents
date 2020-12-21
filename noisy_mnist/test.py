76762ac7fb655b9703cf588b396bf1a2256ba7c2
Augustine97 <augustine.mavor-parker.15@ucl.ac.uk>
2020-12-21 01:10:58 +0000

    some tests

--- noisy_mnist/test.py
+++ noisy_mnist/test.py
@@ -1,5 +1,45 @@
+import numpy as np
 import pytest
+from noisy_mnist_aleatoric_uncertainty_for_poster import NoisyMnistEnv
 
 
 @pytest.fixture
 def noisy_mnist_env():
+    mnist_env = NoisyMnistEnv("train", 0, 2)
+    return mnist_env
+
+
+def check_count_of_classes(x_arr, y_arr):
+    same = 0
+    not_same = 0
+    for i, _ in enumerate(x_arr):
+        if np.array_equal(x_arr[i], y_arr[i]):
+            same += 1
+        else:
+            not_same += 1
+    return same, not_same
+
+
+def test_mnist_env_step(noisy_mnist_env):
+    import math
+
+    x_arr, y_arr = noisy_mnist_env.step()
+    assert x_arr.shape == y_arr.shape  # make sure batch shapes make sense
+    for i, _ in enumerate(x_arr):  # check batch is completely filled
+        assert np.array_equal(x_arr[i], np.zeros((1, 28 * 28))) == False
+        assert np.array_equal(y_arr[i], np.zeros((1, 28 * 28))) == False
+        assert np.array_equal(np.zeros((1, 28 * 28)), np.zeros((1, 28 * 28))) == True
+    same = 0
+    not_same = 0
+    for _ in range(1000):
+        x_arr, y_arr = noisy_mnist_env.step()
+        same_sample, not_same_sample = check_count_of_classes(x_arr, y_arr)
+        same += same_sample
+        not_same += not_same_sample
+    print("same", same)
+    print("not same", not_same)
+    assert math.isclose(same, not_same, rel_tol=0.2)
+
+
+def test_mnist_env_random_sample_of_number(noisy_mnist_env):
+    pass
