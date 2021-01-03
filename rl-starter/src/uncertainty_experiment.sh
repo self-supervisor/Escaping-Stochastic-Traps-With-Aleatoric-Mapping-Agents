4486c8d11d32db3f0b3463db1e04516118c795ac
Augustine97 <augustine.mavor-parker.15@ucl.ac.uk>
2021-01-03 13:55:03 +0000

    empty results

--- rl-starter/src/uncertainty_experiment.sh
+++ rl-starter/src/uncertainty_experiment.sh
@@ -5,13 +5,13 @@ rm *mp4
 frames_before_resets=(8)
 #environments tried: MiniGrid-KeyCorridorS6R3-v0, MiniGrid-DoorKey-5x5-v0, MiniGrid-DoorKey-6x6-v0, MiniGrid-DoorKey-8x8-v0
 # MiniGrid-DoorKey-16x16-v0, MiniGrid-MultiRoom-N6-v0, MiniGrid-KeyCorridorS6R3-v0, MiniGrid-SimpleCrossingS11N5-v0
-environment=MiniGrid-KeyCorridorS6R3-v0 #, #MiniGrid-FourRooms-v0
+environment=MiniGrid-Empty-6x6-v0 #, #MiniGrid-FourRooms-v0
 
 randomise_env=False
-frames=50000
+frames=1000000
 uncertainty_budget=0.0005
-random_seeds=(85 86 87 88 89)
-#random_seeds=(1 2 3)
+#random_seeds=(85 86 87 88 89)
+random_seeds=(1 2 3)
 
 for frames_before_reset in ${frames_before_resets[@]}; do
 
@@ -31,7 +31,7 @@ for frames_before_reset in ${frames_before_resets[@]}; do
             for a_noisy_tv in ${noisy_tv[@]}; do
                 for a_curiosity in ${curiosity[@]}; do
                     environment_seed=$random_seed
-                    python3 -m scripts.train --algo a2c --random_action True --visualizing $visualizing --normalise_rewards True --env $environment --model frames_${frames_before_reset}_noisy_tv_${a_noisy_tv}_random_action_${random_action}_random_seed_${random_seed}_coefficient_${uncertainty_budget}_${environment} --save-interval $save_interval --frames $frames --seed $random_seed --uncertainty $a_uncertainty --noisy_tv $a_noisy_tv --curiosity $a_curiosity --randomise_env $randomise_env --environment_seed $environment_seed --icm_lr $icm_lr --reward_weighting $reward_weighting --frames_before_reset $frames_before_reset & 
+                    python3 -m scripts.train --algo a2c --random_action True --visualizing $visualizing --normalise_rewards True --env $environment --model frames_${frames_before_reset}_noisy_tv_${a_noisy_tv}_random_action_${random_action}_random_seed_${random_seed}_coefficient_${uncertainty_budget}_${environment} --save-interval $save_interval --frames $frames --seed $random_seed --uncertainty $a_uncertainty --noisy_tv $a_noisy_tv --curiosity $a_curiosity --randomise_env $randomise_env --environment_seed $environment_seed --icm_lr $icm_lr --reward_weighting $reward_weighting --frames_before_reset $frames_before_reset  
                 done
             done
         done
@@ -53,7 +53,7 @@ for frames_before_reset in ${frames_before_resets[@]}; do
             for a_noisy_tv in ${noisy_tv[@]}; do
                 for a_curiosity in ${curiosity[@]}; do
                     environment_seed=$random_seed
-                    python3 -m scripts.train --algo a2c --random_action False --visualizing $visualizing --normalise_rewards True --env $environment --model frames_${frames_before_reset}_noisy_tv_${a_noisy_tv}_curiosity_${a_curiosity}_uncertainty_${a_uncertainty}_random_seed_${random_seed}_coefficient_${uncertainty_budget}_${environment} --save-interval $save_interval --frames $frames --seed $random_seed --uncertainty $a_uncertainty --noisy_tv $a_noisy_tv --curiosity $a_curiosity --randomise_env $randomise_env --environment_seed $environment_seed --icm_lr $icm_lr --reward_weighting $reward_weighting --frames_before_reset $frames_before_reset & 
+                    python3 -m scripts.train --algo a2c --random_action False --visualizing $visualizing --normalise_rewards True --env $environment --model frames_${frames_before_reset}_noisy_tv_${a_noisy_tv}_curiosity_${a_curiosity}_uncertainty_${a_uncertainty}_random_seed_${random_seed}_coefficient_${uncertainty_budget}_${environment} --save-interval $save_interval --frames $frames --seed $random_seed --uncertainty $a_uncertainty --noisy_tv $a_noisy_tv --curiosity $a_curiosity --randomise_env $randomise_env --environment_seed $environment_seed --icm_lr $icm_lr --reward_weighting $reward_weighting --frames_before_reset $frames_before_reset  
                 done
             done
         done
@@ -72,7 +72,7 @@ for frames_before_reset in ${frames_before_resets[@]}; do
             for a_noisy_tv in ${noisy_tv[@]}; do
                 for a_curiosity in ${curiosity[@]}; do
                     environment_seed=$random_seed
-                    python3 -m scripts.train --algo a2c --visualizing $visualizing --random_action False --normalise_rewards $normalise_reward --env $environment --model frames_${frames_before_reset}_noisy_tv_${a_noisy_tv}_curiosity_${a_curiosity}_uncertainty_${a_uncertainty}_random_seed_${random_seed}_coefficient_${uncertainty_budget}_${environment} --icm_lr $icm_lr --reward_weighting $reward_weighting --save-interval $save_interval --frames $frames --seed $random_seed --uncertainty $a_uncertainty --noisy_tv $a_noisy_tv --curiosity $a_curiosity --randomise_env $randomise_env --environment_seed $environment_seed --frames_before_reset $frames_before_reset & 
+                    python3 -m scripts.train --algo a2c --visualizing $visualizing --random_action False --normalise_rewards $normalise_reward --env $environment --model frames_${frames_before_reset}_noisy_tv_${a_noisy_tv}_curiosity_${a_curiosity}_uncertainty_${a_uncertainty}_random_seed_${random_seed}_coefficient_${uncertainty_budget}_${environment} --icm_lr $icm_lr --reward_weighting $reward_weighting --save-interval $save_interval --frames $frames --seed $random_seed --uncertainty $a_uncertainty --noisy_tv $a_noisy_tv --curiosity $a_curiosity --randomise_env $randomise_env --environment_seed $environment_seed --frames_before_reset $frames_before_reset  
                 done
             done
         done
@@ -91,7 +91,7 @@ for frames_before_reset in ${frames_before_resets[@]}; do
             for a_noisy_tv in ${noisy_tv[@]}; do
                 for a_curiosity in ${curiosity[@]}; do
                     environment_seed=$random_seed
-                    python3 -m scripts.train --algo a2c --visualizing $visualizing --random_action False --normalise_rewards $normalise_reward --env $environment --model frames_${frames_before_reset}_noisy_tv_${a_noisy_tv}_curiosity_${a_curiosity}_uncertainty_${a_uncertainty}_random_seed_${random_seed}_coefficient_${uncertainty_budget} --icm_lr $icm_lr --reward_weighting $reward_weighting --save-interval $save_interval --frames $frames --seed $random_seed --uncertainty $a_uncertainty --noisy_tv $a_noisy_tv --curiosity $a_curiosity --randomise_env $randomise_env --environment_seed $environment_seed --frames_before_reset $frames_before_reset & 
+                    python3 -m scripts.train --algo a2c --visualizing $visualizing --random_action False --normalise_rewards $normalise_reward --env $environment --model frames_${frames_before_reset}_noisy_tv_${a_noisy_tv}_curiosity_${a_curiosity}_uncertainty_${a_uncertainty}_random_seed_${random_seed}_coefficient_${uncertainty_budget} --icm_lr $icm_lr --reward_weighting $reward_weighting --save-interval $save_interval --frames $frames --seed $random_seed --uncertainty $a_uncertainty --noisy_tv $a_noisy_tv --curiosity $a_curiosity --randomise_env $randomise_env --environment_seed $environment_seed --frames_before_reset $frames_before_reset  
                 done
             done
         done
