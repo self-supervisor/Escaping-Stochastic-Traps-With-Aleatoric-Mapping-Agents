3df2e76488a80c9c8a4b565910f83b6207752d61
Augustine97 <augustine.mavor-parker.15@ucl.ac.uk>
2021-03-03 01:04:00 +0000

    very nice results for 4 rooms

--- rl-starter/src/uncertainty_experiment.sh
+++ rl-starter/src/uncertainty_experiment.sh
@@ -5,10 +5,10 @@ rm *mp4
 frames_before_resets=(8000000000000)
 #environments tried: MiniGrid-KeyCorridorS6R3-v0, MiniGrid-DoorKey-5x5-v0, MiniGrid-DoorKey-6x6-v0, MiniGrid-DoorKey-8x8-v0
 # MiniGrid-DoorKey-16x16-v0, MiniGrid-MultiRoom-N6-v0, MiniGrid-KeyCorridorS6R3-v0, MiniGrid-SimpleCrossingS11N5-v0
-environment=MiniGrid-MultiRoom-N4-S5-v0 #MiniGrid-KeyCorridorS6R3-v0 #MiniGrid-MultiRoom-N4-S5-v0 #, #MiniGrid-FourRooms-v0
+environment=MiniGrid-MultiRoom-N6-v0 #MiniGrid-MultiRoom-N4-S5-v0 #MiniGrid-KeyCorridorS6R3-v0 #MiniGrid-MultiRoom-N4-S5-v0 #, #MiniGrid-FourRooms-v0
 
 randomise_env=False
-frames=1000000
+frames=10000000
 uncertainty_budget=0.0005
 random_seeds=(85 86 87 88 89)
 #random_seeds=(1)
